b5f8fc5b9f84814d60058ecdd5bb83b851092104
Robinlp <Robinlp@users.noreply.github.com>
2019-03-01 13:36:02 +0100

    init

---
+++ projekt_part1/src/Projekt_part1.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Robin
+ * @author Kim
+ * @author Jeppe
+ */
+public class Projekt_part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

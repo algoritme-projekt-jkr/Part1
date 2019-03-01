22bd8c61789433434d0dc6cf22e158e6581ceffc
Robinlp <Robinlp@users.noreply.github.com>
2019-03-01 13:45:44 +0100

    PQHeap

---
+++ projekt_part1/src/PQHeap.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author Robin
+ */
+public class PQHeap implements PQ{
+
+    @Override
+    public Element extractMin() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insert(Element e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

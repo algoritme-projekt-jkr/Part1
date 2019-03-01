5be706393df348ac75d0be17b112464fbee14425
Robinlp <Robinlp@users.noreply.github.com>
2019-03-01 15:29:13 +0100

    kim

--- projekt_part1/src/PQHeap.java
+++ projekt_part1/src/PQHeap.java
@@ -3,6 +3,7 @@
  *
  * @author Robin Lausten Petersen   - ropet17
  * @author Jeppe Enevold Jensen     - jeppj17
+ * @author Kim Christensen          - kichr17
  */
 public class PQHeap implements PQ{
 

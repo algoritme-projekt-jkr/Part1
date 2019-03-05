394f4d65a176b20dc8631073ac59ed73ae40371d
Robinlp <Robinlp@users.noreply.github.com>
2019-03-05 15:01:31 +0100

    parent, left, right

--- projekt_part1/src/PQHeap.java
+++ projekt_part1/src/PQHeap.java
@@ -2,25 +2,40 @@
 /**
  *
  * @author Robin Lausten Petersen   - ropet17
- * @author Jeppe Enevold Jensen     - jeppj17
- * @author Kim Christensen          - kichr17
+ * @author Jeppe Enevold Jensen - jeppj17
+ * @author Kim Christensen - kichr17
  */
-public class PQHeap implements PQ{
-    
-    Element[]elements;
-    
-    public PQHeap(int i){
+public class PQHeap implements PQ {
+
+    private Element[] elements;
+    private int heapSize;
+
+    public PQHeap(int i) {
         elements = new Element[i];
+        heapSize = 0;
     }
-    
+
     @Override
     public Element extractMin() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Element min = elements[0];
+
     }
 
     @Override
     public void insert(Element e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    private int parent(int i) {
+        return (int) Math.floor(i / 2);
+    }
+
+    private int left(int i) {
+        return (i * 2) + 1;
+    }
+
+    private int right(int i) {
+        return (i + 1) * 2;
+    }
+
 }
